02d148f2360327509c074803fa21605242d40b3f
AndreyBeny <dubina2017@yandex.ru>
2022-05-25 10:06:40 +0300

    My branch

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,8 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("How do you do?");
+        System.out.println("How do you do?");
+        System.out.println("How do you do?");
+        System.out.println("How do you do?");
     }
 }
